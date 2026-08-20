8a37410eb3fece692b23a6f59b981f4c6fc9fc77
Jose <joxzes@pm.me>
2026-08-20 01:19:36 -0500

    queue: Add FIFO structure

--- drivers/queue/queue.h
+++ drivers/queue/queue.h
@@ -3,6 +3,15 @@
 
 #include "nrf52840.h"
 
+#define SIZE 8
+
+struct fifo {
+	uint8_t buffer[SIZE];
+	uint8_t head;
+	uint8_t tail;
+	uint8_t count;
+};
+
 void fifo_init(struct fifo *fifo);
 int fifo_push(struct fifo *fifo, uint8_t data);
 int fifo_pop(struct fifo *fifo, uint8_t *data);
